a3c91d6091d8613f787e1150cd1a4d16e77ee7a9
Nicolas <nicolasmartin89@gmail.com>
2024-08-13 11:30:39 -0300

    Add BrowserContext for manage sesions.

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,17 +1,26 @@
-import { BeforeAll, AfterAll } from "@cucumber/cucumber";
-import { Browser, chromium, Page } from "@playwright/test";
+import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
+import { Browser, BrowserContext, chromium, Page } from "@playwright/test";
 import { pageFixture } from "./pageFixtures";
 
 let browser: Browser;
 let page: Page;
+let context: BrowserContext;
 
 BeforeAll(async () => {
   browser = await chromium.launch({ headless: false });
-  page = await browser.newPage();
+});
+
+Before(async () => {
+  context = await browser.newContext();
+  page = await context.newPage();
   pageFixture.page = page;
 });
 
-AfterAll(async () => {
+After(async () => {
   await pageFixture.page.close();
+  await context.close();
+});
+
+AfterAll(async () => {
   await browser.close();
 });
